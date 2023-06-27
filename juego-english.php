aaff1bcb5831c2ed57c110248777162a4db438aa
KlkSugoi <137863412+KlkSugoi@users.noreply.github.com>
2023-06-27 01:02:05 -0600

    Commit inicial

---
+++ juego-english.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <link rel="stylesheet" href="juego.css">
+    <title>Game words</title>
+     <link rel="icon" href="img/Cecybara%20.png" > <!----Logo en pestaña---->
+<!--Poner si o si------->
+
+</head>
+<body>
+    
+    <h1 class="Titulo-uno">Completa</h1><br>
+    <form class="pregunta-uno" method="get" action="juego-english.php">
+        <h3 class="pregunta-parte-uno">Avalos has</h3><input class="caja-respuesta-uno" type="text" value=""><h3 class="pregunta-parte-dos">Karol many times</h3>
+        <input class="subir-boton" type="submit" value="Subir">
+    </form>
+    
+    </body>
+</html>
